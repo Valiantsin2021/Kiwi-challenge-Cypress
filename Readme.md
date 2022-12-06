ff39cb357b7a44d12e977de7bf9748a9c34dfc95
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2022-12-06 21:50:50 +0100

    chenge viewport

--- Readme.md
+++ Readme.md
@@ -1,1 +1,12 @@
 ## Kiwi challenge
+
+
+User path tests UI and shortcuted for searching a flight and checking the price is visible
+
+## Used:
+
+1. Mochawesome reporter
+2. GitHub Actions (push hook)
+3. Report deployment on GitHub Pages
+
+report link https://valiantsin2021.github.io/Kiwi-challenge-Cypress/
--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://www.kiwi.com/',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     env: {
       token:
         'E2E98QA6vGtKlaNT6T7tTclrKojUSxVZXQq9bxPKDM9Dy1wK0whnX412c5pGcie3NYMLwGjNkOUNwuQo4a51Qh6s-S25RFIU9FlbgN2x2qyg6sMsrcKEKR2b-Vi_vabtqdiFowwFMODrljmnUUfOrAld3QeN3BXYnHxe2OeEpt_y8aPrxFAeB1lzzeNR71AcDSx4rn_2yq4oNxcf5wEpeI8e6N4xZ39zDo0E2-iR3EMIKeyURdW3JPOybBIroD5rjFz-Hy2oU9sMB4Kp7Zx3T9gToX3bI4NcrheKfGFHeyVow4CAM9C_KFjH8N2aONlqv2iV4RWSFItbfzSwD2ltrlSsIRvF9pHKl68U_z2EJ5V5skumnb7u7LNPCcriZUweekIkhouIih4vYgvAV8iLrL6eDqqrf4VrjqNqZeh9eti1sGviOwx-Wbf7yCq_f0xGxuQbHjiY2l4NfVojsMs6kIw%3D%3D'
