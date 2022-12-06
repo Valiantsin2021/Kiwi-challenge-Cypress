143188cc0a59eac831d9003454370e7665b6822e
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2022-12-06 21:43:44 +0100

    add Kiwi challenge test

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,74 @@
+describe('empty spec', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.get('#cookies_accept').click()
+  })
+  it('Performs UI flight search on Kiwi agregator and checks the prices are shown', () => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false
+    })
+    cy.get('[data-test="PlacePickerInputPlace-close"]').click()
+    cy.get('[data-test="SearchField-input"').eq(0).type('Malaga').blur()
+    cy.contains(
+      '[data-test="PlacePickerRow-wrapper"] div',
+      'Málaga, Spain'
+    ).click({ force: true })
+    cy.get('[data-test="SearchField-input"').eq(1).type('Stockholm').blur()
+    cy.contains(
+      '[data-test="PlacePickerRow-wrapper"] div',
+      'Stockholm, Sweden'
+    ).click({ force: true })
+    cy.window().then(win => {
+      cy.stub(win, 'open')
+        .callsFake((url, target) => {
+          expect(target).to.be.undefined
+          return win.open.wrappedMethod.call(win, url, '_self')
+        })
+        .as('open')
+      cy.get('[data-test="LandingSearchButton"]').click()
+      cy.get('@open').should(
+        'have.been.calledOnceWithExactly',
+        '/en/search/results/malaga-spain/stockholm-sweden'
+      )
+      cy.go('back')
+      cy.get('[data-test="LandingSearchButton"]').click()
+      cy.contains('div', 'Select').eq(0).click()
+      cy.get('[data-test="MagicLogin-GuestTextLink"]').click()
+    })
+  })
+  it(`Uses request to shortcut the search flight on Kiwi agregator`, () => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      return false
+    })
+    cy.visit({
+      url: 'https://www.kiwi.com/booking',
+      qs: {
+        token: Cypress.env('token'),
+        holdBags: 1
+      }
+    })
+    //assert price is visible
+    cy.get('.ReservationBill-item-price')
+      .should('be.visible')
+      .should('not.be.empty')
+    cy.get('[data-test="bookingBillCheckedBaggage"] > p')
+      .eq(0)
+      .should('be.visible')
+      .and('have.text', 'Checked baggage')
+    cy.get('[data-test="bookingBillCheckedBaggage"] span.length-7')
+      .eq(0)
+      .should('be.visible')
+      .should('not.be.empty')
+    cy.get('[data-test="Baggage-AddBlueRibbonBag"]').check({ force: true })
+    cy.get('[data-test="bookingBillBlueribbonBags"] > p')
+      .eq(0)
+      .should('be.visible')
+      .and('contain.text', 'Lost baggage protection')
+    cy.get('[data-test="bookingBillBlueribbonBags"] p')
+      .eq(1)
+      .should('be.visible')
+      .should('not.be.empty')
+  })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,24 @@
+Cypress.Commands.overwrite('log', (log, message, ...args) => {
+  // print the to Cypress Command Log
+  // to preserve the existing functionality
+  log(message, ...args)
+  // send the formatted message down to the Node
+  // callback in the cypress.config.js to be printed to the terminal
+  cy.task('print', [message, ...args].join(', '), { log: false })
+})
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
